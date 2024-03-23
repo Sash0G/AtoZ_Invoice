fe7a3beb28a74379df3c5c182330d1c65ffc17e8
Sasho_G <100850331+Sash0G@users.noreply.github.com>
2024-03-23 19:08:14 +0200

    First commit

--- Python_Code/Builder.bat
+++ Python_Code/Builder.bat
@@ -4,6 +4,7 @@ xcopy .\config.txt     .\dist\AtoZ_Invoice\_internal
 xcopy .\data.db     .\dist\AtoZ_Invoice\_internal
 xcopy .\template.xlsx     .\dist\AtoZ_Invoice\_internal
 RMDIR /S /Q .\build
+signer /sign FlyTechVideos .\dist\AtoZ_Invoice\AtoZ_Invoice.exe
 for /f "tokens=2 delims==" %%a in ('wmic OS Get localdatetime /value') do set "dt=%%a"
 set "YYYY=%dt:~0,4%" & set "MM=%dt:~4,2%" & set "DD=%dt:~6,2%"
 rename .\dist\AtoZ_Invoice AtoZ_Invoice_%DD%.%MM%.%YYYY%
